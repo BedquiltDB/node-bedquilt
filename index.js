d1f0304b30a93cf0b378c19e586bb99a7ea16b58
Shane Kilkelly <shane@kilkelly.me>
2015-04-23 19:17:35 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+exports.BedquiltClient = function(connectionString) {
+    this.connectionString = connectionString;
+};
---
+++ test/test.js
@@ -0,0 +1,12 @@
+var assert = require("assert");
+var bq = require('../index.js');
+
+
+describe('Basic test', function() {
+    describe('#BedquiltClient()', function() {
+        it('should return 1', function() {
+            var client = new bq.BedquiltClient("a");
+            assert.notEqual(client, null);
+        });
+    });
+});
