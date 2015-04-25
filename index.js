f5bfaddb348f9b4cc6949992b95d453047964796
Shane Kilkelly <shane@kilkelly.me>
2015-04-25 17:14:56 +0100

    Start exploring.

--- index.js
+++ index.js
@@ -3,7 +3,20 @@
 
 var pg = require('pg');
 
-exports.BedquiltClient = function(connectionString) {
+var BedquiltClient = function(connectionString) {
   this.connectionString = connectionString;
-  this.connection = new pg.Client(connectionString);
+  this._query = function(collectionName, queryDoc) {
+    // pass
+  };
+  this.collection = function(collectionName) {
+    return new BedquiltCollection(this, collectionName);
+  };
 };
+
+var BedquiltCollection = function(client, collectionName) {
+  this.client = client;
+  this.collectionName = collectionName;
+};
+
+exports.BedquiltClient = BedquiltClient;
+exports.BedquiltCollection = BedquiltCollection;
--- test/test.js
+++ test/test.js
@@ -4,12 +4,14 @@
 var should = require("should");
 var bq = require('../index.js');
 
+var connectionString = 'postgres://localhost/bedquilt_test';
+
 describe('Basic test', function() {
   describe('#BedquiltClient()', function() {
     it('should initialise', function() {
-      var client = new bq.BedquiltClient("a");
+      var client = new bq.BedquiltClient(connectionString);
       should.notEqual(client, null);
-      should.notEqual(client.connection, undefined);
+      should.equal(client.connectionString, connectionString);
     });
   });
 });
