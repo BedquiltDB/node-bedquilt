b80505e2ede19c29bb5f2d1e304addc5b2685ade
Shane Kilkelly <shane@kilkelly.me>
2015-04-26 15:58:59 +0100

    add stubs

--- index.js
+++ index.js
@@ -11,11 +11,17 @@ var BedquiltClient = function(connectionString) {
   this.collection = function(collectionName) {
     return new BedquiltCollection(this, collectionName);
   };
+  this.listCollections = function() {
+    // pass
+  };
 };
 
 var BedquiltCollection = function(client, collectionName) {
   this.client = client;
   this.collectionName = collectionName;
+  this._query = function(queryDoc) {
+    // pass
+  };
 };
 
 exports.BedquiltClient = BedquiltClient;
