07617c7e5a6ffa15b910f289b278171dc38bf9d6
Shane Kilkelly <shane@kilkelly.me>
2015-05-02 11:39:43 +0100

    Make a testutils module.

--- test/test.js
+++ test/test.js
@@ -3,15 +3,15 @@
 
 var should = require("should");
 var bq = require('../index.js');
+var testutils = require('./testutils.js');
 
-var connectionString = 'postgres://localhost/bedquilt_test';
 
 describe('Basic test', function() {
   describe('#BedquiltClient()', function() {
     it('should initialise', function() {
-      var client = new bq.BedquiltClient(connectionString);
+      var client = new bq.BedquiltClient(testutils.connectionString);
       should.notEqual(client, null);
-      should.equal(client.connectionString, connectionString);
+      should.equal(client.connectionString, testutils.connectionString);
     });
   });
 });
---
+++ test/testutils.js
@@ -0,0 +1,5 @@
+/* jslint node: true */
+"use strict";
+
+exports.connectionString = 'postgres://localhost/bedquilt_test';
+
