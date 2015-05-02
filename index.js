642477c5cd4d854525c09cac9ef5eaf1fcf1ab91
Shane Kilkelly <shane@kilkelly.me>
2015-05-02 13:09:44 +0100

    Scaffolding.

--- index.js
+++ index.js
@@ -3,16 +3,28 @@
 
 var pg = require('pg');
 
-function BedquiltClient() {
-
-
-};
+function BedquiltClient() {};
 
 BedquiltClient.connect = function(connectionString, callback) {
   var db = {
-    connectionString: connectionString
+    connectionString: connectionString,
+    collection: function(collectionName) {
+      return new BedquiltCollection(this, collectionName);
+    },
+    _query: function(collectionName, queryDoc, callback) {
+      callback(null, null);
+    }
   };
   callback(null, db);
 };
+
+function BedquiltCollection(db, collectionName) {
+  this.db = db;
+  this.collectionName = collectionName;
+  this._query = function(queryDoc, callback) {
+    db._query(this.collectionName, queryDoc, callback);
+  };
+};
+
 
 exports.BedquiltClient = BedquiltClient;
