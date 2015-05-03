ee18ec5d3d10529644343b01f69566cf599a22b0
Shane Kilkelly <shane@kilkelly.me>
2015-05-03 11:50:42 +0100

    Start fixing tests so that they actually run.

--- test/test.js
+++ test/test.js
@@ -11,29 +11,32 @@ describe('Basic test', function() {
 
   describe('#BedquiltClient.connect()', function() {
 
-    it('should connect', function() {
+    it('should connect', function(done) {
       BedquiltClient.connect(_cs, function(err, db) {
         should.equal(err, null);
         should.notEqual(db, null);
         should.equal(db.connectionString, _cs);
+        done();
       });
     });
 
-    it('should allow us to query', function() {
+    it('should allow us to query', function(done) {
       BedquiltClient.connect(_cs, function(err, db) {
         db._query('select 1', [], function(err, result) {
           should.equal(err, null);
           should.equal(result, 1);
+          done();
         });
       });
     });
 
-    it('should list collections', function() {
+    it('should list collections', function(done) {
       // with no collections
       BedquiltClient.connect(_cs, function(err, db) {
         db.listCollections(function(err, result) {
           should.equal(err, null);
           should.equal(result, 0);
+          done();
         });
       });
     });
