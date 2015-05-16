49e26555a1591c7cbd02aec0d0aedb6215a848bc
Shane Kilkelly <shane@kilkelly.me>
2015-05-16 11:04:25 +0100

    Test remove

--- test/test_writes.js
+++ test/test_writes.js
@@ -8,6 +8,49 @@ var async = require('async');
 
 describe('BedquiltCollection write ops', function() {
 
+  describe('BedquiltCollection#remove()', function() {
+    beforeEach(testutils.cleanDatabase);
+    afterEach(testutils.cleanDatabase);
+
+    it('should do nothing on empty collection', function(done) {
+      testutils.connect(function(err, db) {
+        var things = db.collection('things');
+        things.remove({tag: 'aa'}, function(err, result) {
+          should.equal(result, 0);
+          done();
+        });
+      });
+    });
+
+    it('should remove documents from collection', function(done) {
+      testutils.connect(function(err, db) {
+        var things = db.collection('things');
+
+        async.series([
+          function(callback) {
+            things.insert({tag: 'aa'}, callback);
+          },
+          function(callback) {
+            things.insert({tag: 'bb'}, callback);
+          },
+          function(callback) {
+            things.insert({tag: 'aa'}, callback);
+          },
+        ], function(err, results) {
+          things.remove({tag: 'aa'}, function(err, result) {
+            should.equal(result, 2);
+            things.count({}, function(err, result) {
+              should.equal(1, result);
+              done();
+            });
+          });
+        });
+      });
+    });
+
+
+  });
+
   describe('BedquiltCollection#insert()', function() {
     beforeEach(testutils.cleanDatabase);
     afterEach(testutils.cleanDatabase);
