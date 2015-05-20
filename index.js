dd021575ebf33a1b0875a44713338ab057a9e32d
Shane Kilkelly <shane@kilkelly.me>
2015-05-20 19:17:34 +0100

    move some things around

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 "use strict";
 
 var pg = require('pg');
+var BedquiltCollection = require('./lib/bedquiltCollection.js');
+var project = require('./lib/utils.js').project;
 
 /**
  * Creates a new BedquiltClient instance.
@@ -77,114 +79,6 @@ BedquiltClient.connect = function(connectionString, callback) {
   };
 
   return callback(null, db);
-};
-
-/**
- * Create a BedquiltCollection instance
- * @constructor
- * @param {BedquiltClient} db The BedquiltClient instance to use
- * @param {string} collectionName Name of collection
- */
-function BedquiltCollection(db, collectionName) {
-  this.db = db;
-  this.collectionName = collectionName;
-
-  this.count = function(queryDoc, callback) {
-    return this.db._query(
-      "select bq_count($1::text, $2::json);",
-      [this.collectionName, queryDoc],
-      project.single('bq_count'),
-      callback
-    );
-  };
-
-  this.insert = function(doc, callback) {
-    return this.db._query(
-      "select bq_insert($1::text, $2::json);",
-      [this.collectionName, doc],
-      project.single('bq_insert'),
-      callback
-    );
-  };
-
-  this.save = function(doc, callback) {
-    return this.db._query(
-      "select bq_save($1::text, $2::json);",
-      [this.collectionName, doc],
-      project.single('bq_save'),
-      callback
-    );
-  };
-
-  this.find = function(queryDoc, callback) {
-    return this.db._query(
-      "select bq_find($1::text, $2::json);",
-      [this.collectionName, queryDoc],
-      project.column('bq_find'),
-      callback
-    );
-  };
-
-  this.findOne = function(queryDoc, callback) {
-    return this.db._query(
-      "select bq_find_one($1::text, $2::json);",
-      [this.collectionName, queryDoc],
-      project.single('bq_find_one'),
-      callback
-    );
-  };
-
-  this.findOneById = function(_id, callback) {
-    return this.db._query(
-      "select bq_find_one_by_id($1::text, $2::text);",
-      [this.collectionName, _id],
-      project.single('bq_find_one_by_id'),
-      callback
-    );
-  };
-
-  this.remove = function(queryDoc, callback) {
-    return this.db._query(
-      "select bq_remove($1::text, $2::json)",
-      [this.collectionName, queryDoc],
-      project.single('bq_remove'),
-      callback
-    );
-  };
-
-  this.removeOne = function(queryDoc, callback) {
-    return this.db._query(
-      "select bq_remove_one($1::text, $2::json)",
-      [this.collectionName, queryDoc],
-      project.single('bq_remove_one'),
-      callback
-    );
-  };
-
-  this.removeOneById = function(_id, callback) {
-    return this.db._query(
-      "select bq_remove_one_by_id($1::text, $2::text)",
-      [this.collectionName, _id],
-      project.single('bq_remove_one_by_id'),
-      callback
-    );
-  };
-};
-
-
-var project = {
-  column: function(columnName) {
-    return function(result) {
-      return result.rows.map(function(row) {
-        return row[columnName];
-      });
-    };
-  },
-  single: function(columnName) {
-    return function(result) {
-      return result.rows[0][columnName];
-    };
-  }
 };
 
 
