fa78f050e8a00982c887f900e1f8f748486cd6cd
Shane Kilkelly <shane@kilkelly.me>
2015-05-26 19:00:37 +0100

    Tidy up a bit.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-/*jslint node: true*/
-/*global require, exports*/
+/*jshint node: true*/
+/*global require exports*/
 "use strict";
 
-var BedquiltClient= require('./lib/client.js');
+var BedquiltClient = require('./lib/client.js');
 var BedquiltCollection = require('./lib/collection.js');
-
 
 exports.BedquiltClient = BedquiltClient;
 exports.BedquiltCollection = BedquiltCollection;
